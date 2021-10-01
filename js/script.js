24ba20cccbd5a8faaa43ea737c364ea595150fa0
vasapavlish15 <79367509+vasapavlish15@users.noreply.github.com>
2021-10-01 21:13:43 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/*
+function openMenu() {
+    document.querySelector(".side-bar").classList.toggle('active');
+}
+*/
+
+/*
+let btn = document.querySelector('.btn'),
+    elem = document.querySelector('.sub_box');
+
+
+function myAnim() {
+    let post = 0;
+
+    let id = setInterval(frame, 10);
+    function frame() {
+        if (post == 250) {
+            clearInterval(id);
+        } else {
+            post++;
+            elem.style.top = post + 'px';
+            elem.style.left = post + 'px';
+        }
+    }
+}
+
+btn.addEventListener("click", myAnim);
+*/
